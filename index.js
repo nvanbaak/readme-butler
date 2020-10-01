004793aff44a93f56f6e60ced5a49702ee0a77b7
Nikolai <nvanbaak@gmail.com>
2020-10-01 10:48:50 -0700

    added inquirer

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import statements
+var inq = require("inquirer");
+
+// Inquirer call to get information
+inq.prompt([
+    {
+        message:"What is the name of the project?",
+        type:"input",
+        name:"projName"
+    } , {
+        message:"What problem does it target?",
+        type:"input",
+        name:"projProb"
+    } , {
+        message:"What does it do?",
+        type:"input",
+        name:"projDesc"
+    }
+])
