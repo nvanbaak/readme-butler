fae911e3a863df280fd8dba4bfb19bfced26fa68
Nikolai <nvanbaak@gmail.com>
2020-10-09 22:30:11 -0700

    refactored textGen export statement

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-# fghjfg
-    
-    ## Table of Contents
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Credits](#credits)
-    * [License](#license)
-    
-    ## Description
-    
-    asdad  dfn
-    
-    ## Installation
-    
-    
-    
-    ## Usage
+# oyiuoyiuy
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Description
+
+xcvxcvcx  sdfsdfsdf
+
+## Installation
+
+oyiuoyiuy can be installed by entering the following command in the app's directory:
+```
+npm install
+```
+
+This should install the necessary npm packages and allow the app to run.
+
+## Usage
 
--- textGen.js
+++ textGen.js
@@ -10,11 +10,11 @@ function generateReadmeTop(name, description, install) {
     if(!install) {
         // If the user didn't enter special instructions we use the default
         installStr = `${name} can be installed by entering the following command in the app's directory:
-        \`\`\`
-        npm install
-        \`\`\`
-        
-        This should install the necessary npm packages and allow the app to run.`;
+\`\`\`
+npm install
+\`\`\`
+
+This should install the necessary npm packages and allow the app to run.`;
     } else {
         // If the user entered specific instructions we use those instead
         installStr = install;
@@ -23,27 +23,29 @@ function generateReadmeTop(name, description, install) {
 
     // Build readme page
     outputStr = `# ${name}
-    
-    ## Table of Contents
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Credits](#credits)
-    * [License](#license)
-    
-    ## Description
-    
-    ${description}
-    
-    ## Installation
-    
-    ${installStr}
-    
-    ## Usage\n\n`
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Description
+
+${description}
+
+## Installation
+
+${installStr}
+
+## Usage\n\n`
     
 
     // Return completed readme
     return outputStr;
 }
 
-module.exports.generateReadmeTop = generateReadmeTop;
+module.exports = {
+    generateReadmeTop
+}
