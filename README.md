641ec63a58c725467d5894acee8d6acab92841b3
Nikolai <nvanbaak@gmail.com>
2020-10-09 20:31:33 -0700

    added description section

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Ham
+# Killigan
 
+## Table of Contents
+* [Description](#description)
+
+## Description
+
+The Imperial Guard are full of cowardly guardsmen who want to abandon their posts.  Killigan solves this problem by shooting every guardsman who deserts in order to build morale.
--- index.js
+++ index.js
@@ -10,30 +10,40 @@ inq.prompt([
         message:"What is the name of the project?",
         type:"input",
         name:"projName"
+    } , {
+        message:"What problem does it target?",
+        type:"input",
+        name:"projProb"
+    } , {
+        message:"What does it do?",
+        type:"input",
+        name:"projDesc"
+    } , {
+        message:"What steps does the user need to take to install the project? (leave blank if none)",
+        name:"install"
     } 
-    // , {
-    //     message:"What problem does it target?",
-    //     type:"input",
-    //     name:"projProb"
-    // } , {
-    //     message:"What does it do?",
-    //     type:"input",
-    //     name:"projDesc"
-    // } , {
-    //     message:"What steps does the user need to take to install the project? (leave blank if none)",
-    //     name:"install"
-    // } , {
+    //, {
     //     message:"What's the program workflow?",
     //     name:"workflow"
     // }
 ]).then((response) => {
 
-    outputStr = `# ${response.projName}\n\n`
+    // Build readme page
+    outputStr = `# ${response.projName}
 
+## Table of Contents
+* [Description](#description)
+
+## Description
+
+${response.projProb}  ${response.projDesc}`
+
+
+    // Then output to readme file
     fs.writeFile("./readme.md",outputStr,"utf8",function(err) {
         if (err) throw err;
     });
 
-    console.log("Readme complete!");
+    console.log("\nReadme complete!");
 });
 
