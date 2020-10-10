eb45d7ba7aac9fead53e5a836a7e7e3c1d079e9b
Nikolai <nvanbaak@gmail.com>
2020-10-09 20:11:26 -0700

    output function works

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 // Import statements
-var inq = require("inquirer");
+const inq = require("inquirer");
+const fs = require("fs");
+
+let outputStr = "test";
 
 // Inquirer call to get information
-inq.prompt([
-    {
-        message:"What is the name of the project?",
-        type:"input",
-        name:"projName"
-    } , {
-        message:"What problem does it target?",
-        type:"input",
-        name:"projProb"
-    } , {
-        message:"What does it do?",
-        type:"input",
-        name:"projDesc"
-    } , {
-        message:"What steps does the user need to take to install the project? (leave blank if none)",
-        name:"install"
-    } , {
-        message:"What's the program workflow?",
-        name:"workflow"
-    } , {
-        
+// inq.prompt([
+//     {
+//         message:"What is the name of the project?",
+//         type:"input",
+//         name:"projName"
+//     } , {
+//         message:"What problem does it target?",
+//         type:"input",
+//         name:"projProb"
+//     } , {
+//         message:"What does it do?",
+//         type:"input",
+//         name:"projDesc"
+//     } , {
+//         message:"What steps does the user need to take to install the project? (leave blank if none)",
+//         name:"install"
+//     } , {
+//         message:"What's the program workflow?",
+//         name:"workflow"
+//     }
+// ]).then((response) => {
+
+// });
+
+fs.writeFile("./readme.md",outputStr,"utf8",function(err) {
+    if(err) {
+        throw err;
     }
-])
+});
